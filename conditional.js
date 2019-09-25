ef1a9fe356db8250cc5fcfbd1b8a9b5dd7eba7fc
yahboydew <54632699+yahboydew@users.noreply.github.com>
2019-09-25 10:52:06 -0400

    Add files via upload

---
+++ conditional.js
@@ -0,0 +1,86 @@
+// In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+// If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
+
+
+var likesToTravel = true;
+
+if (likesToTravel === true) {
+    console.log("Bon voyage!")
+} else {
+    console.log ("Enjoy your couch!!")
+}
+
+
+var favoriteFood = "apple";
+if (favoriteFood === "fried pickles" || favoriteFood === "pepperoni rolls" ) {
+console.log("Congratulations! You have excellent taste!");
+} else {
+    console.log("Clearly you have not tried fried pickles or pepperoni rolls.")
+}
+
+
+if (1+1>5) {
+    console.log("facts")
+} else {
+    console.log("big bad math")
+} 
+
+
+if(4*5<20) {
+    console.log("math is not hard")
+} else {
+    console.log("wrong")
+}
+
+
+if (6-2>=0) {
+    console.log("u right")
+} else {
+    console.log("nah fam")
+}
+
+
+var birthYear = "2000";
+if (birthYear === "GenZ" || birthYear === "iGen" ) {
+    console.log("ur a millennial.");
+} else  { 
+    console.log( "GenZ is full of young, creative, and hardworking individuals.")
+}
+//need to fix this
+
+
+
+
+var likesDogs = true;
+
+if (likesDogs === true) {
+    console.log ("You're a dog person.")
+} else {
+    console.log ("You're a cat person.")
+}
+
+var time = "12:00"
+   var isHungry = "yes"
+   if(time === "7:30" && isHungry === "yes"){
+       console.log("Time for Breakfast");
+     } else if(time === "12:00" && isHungry === "yes"){
+       console.log("Time for Lunch");
+     } else if(time === "6:00" && isHungry === "yes"){
+       console.log("Time for Dinner");
+     } else if(time === "7:30" && isHungry === "no"){
+       console.log("Have a cookie");
+     } else if(time === "12:00" && isHungry === "no"){
+       console.log("Have a cookie");
+     } else if(time === "6:00" && isHungry === "no"){
+       console.log("Have a cookie");
+     }
+
+
+var grade = 95
+if (grade >= 0 && grade <= 69){console.log("You got an F")}
+if (grade >= 70 && grade <= 76){console.log("You got an D")}
+if (grade >= 77 && grade <= 84){console.log("You got an C")}
+if (grade >= 84 && grade <= 92){console.log("You got an B")}
+if (grade >= 93 && grade <= 100){console.log("You got an A")}
+
+
